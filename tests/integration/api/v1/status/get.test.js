272063cbec72d356fa9b2d01fa688550da1f0130
Marcio Ferreira <analistamarcio@hotmail.com>
2023-12-28 04:21:18 +0000

    Finished `/api/vi/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,4 +4,13 @@ url = `http://localhost:${port}/api/v1`;
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch(`${url}/status`);
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.openned_connections).toEqual(1);
 });
