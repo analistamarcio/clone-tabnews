c1b331d8096d614ec48d6833defb8e6cc14b0765
analistamarcio <analistamarcio@hotmail.com>
2024-02-24 12:31:16 -0800

    handle ssl database connection

--- infra/database.js
+++ infra/database.js
@@ -7,13 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER ?? "nouser",
     database: process.env.POSTGRES_DB ?? "noname",
     password: process.env.POSTGRES_PASSWORD ?? "nopass",
-  });
-  console.log("Postgres's credentials:", {
-    host: process.env.POSTGRES_HOST ?? "nohost",
-    port: process.env.POSTGRES_PORT ?? "noport",
-    user: process.env.POSTGRES_USER ?? "nouser",
-    database: process.env.POSTGRES_DB ?? "noname",
-    password: process.env.POSTGRES_PASSWORD ?? "nopass",
+    ssl: process.env.NODE_ENV !== "development",
   });
 
   try {
